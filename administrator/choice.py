d8561cdc824efb8472529d0d49237cf2af356e09
Kashyap Nasit <kashyapnasit@gmail.com>
2019-04-08 08:22:12 +0530

    Make all models ready

---
+++ administrator/choice.py
@@ -0,0 +1,9 @@
+DEPARTMENT_CHOICES = (
+    (1,'CSED'),
+    (2, 'MED'),
+    (3, 'ChED'),
+    (4, 'CiED'),
+    (5, 'BED'),
+    (6, 'EED'),
+    (7, 'ECED'),
+)
--- allotment/models.py
+++ allotment/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from administrator.models import Batch
+from professor.models import Professor
 
 
-class GroupInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_group_leader = models.BooleanField(default=False)
-#TODO    preference_of_professeor = models.OneTOManyRel(Professor)
-    group_id = models.SmallIntegerField(null=True)
-
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        GroupInfo.objects.create(user=instance)
-    instance.profile.save()
-
+class Group(models.Model):
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
+    group_id = models.SmallIntegerField(primary_key=True)
+    mentor = models.ForeignKey(Professor, models.SET_NULL, null=models.SET_NULL)
